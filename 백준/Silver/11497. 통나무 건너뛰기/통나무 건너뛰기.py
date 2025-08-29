790549ae6ceb442cd9873194ee2e21ab659b8505
JeongWon-CHO <128157441+JeongWon-CHO@users.noreply.github.com>
2025-08-29 17:28:39 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 196 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -7,17 +7,10 @@ for _ in range(T):
     N = int(input().rstrip())
     L = sorted(list(map(int, input().rstrip().split())))
 
-    result = []
-    for i in range(0, N, 2):
-        result.append(L[i])
-
-    idx = N-1 if N % 2 == 0 else N-2
-    for i in range(idx, -1, -2):
-        result.append(L[i])
-
     answer = 0
-    for i in range(N-1):
-        answer = max(abs(result[i] - result[i+1]), answer)
+    for i in range(N-2):
+        answer = max(abs(L[i]-L[i+2]), answer)
 
-    answer = max(abs(result[-1] - result[0]), answer)
+    answer = max(abs(L[N-1]-L[N-2]), abs(L[0]-L[1]), answer)
+
     print(answer)
