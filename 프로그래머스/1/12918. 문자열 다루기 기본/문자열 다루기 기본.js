f6836b5fda114784d46738d39443ada02eaa3b29
JeongWon-CHO <128157441+JeongWon-CHO@users.noreply.github.com>
2025-10-24 14:33:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    for(let i=0; i<s.length; i++) {
+        if (isNaN(s[i])) return false;
+    }
+    
+    if (!(s.length === 4 || s.length === 6)) return false;
+    return true
+}
