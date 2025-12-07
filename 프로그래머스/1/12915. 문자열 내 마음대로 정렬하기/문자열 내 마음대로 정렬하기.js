3bc15cba93c7b1d44220cff732adb6c6cef684cd
JeongWon-CHO <128157441+JeongWon-CHO@users.noreply.github.com>
2025-12-07 18:06:22 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 33.81 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,7 @@
+function solution(strings, n) {
+    strings.sort((a, b) => {
+        if (a[n] === b[n]) return a.localeCompare(b);
+        return a[n].localeCompare(b[n]);
+    });
+    return strings;
+}
